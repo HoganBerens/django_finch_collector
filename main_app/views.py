81869048df765047b1de0701634dd8b1a6ce5b6d
HoganBerens <berenshogan@gmail.com>
2023-08-04 15:17:37 -0500

    create about and finches page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+finches = [
+    {"name": "Amy", "species": "American Goldfinch", "description": "Yellow with black wings", "age": 2},
+    {"name": "Paul", "species": "House finch", "description": "Grey with red head and chest", "age": 5},
+    {"name": "Clint", "species": "Hawfinch", "description": "Orange with brown and black wings", "age": 1}
+]
+
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def finches_index(request):
+    return render(request, "finches/index.html", {"finches": finches})
