ac834cb0980ab47c9dfdc10bc9e343ac2456ec17
HoganBerens <berenshogan@gmail.com>
2023-08-07 10:50:09 -0500

    add finch details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
-    {"name": "Amy", "species": "American Goldfinch", "description": "Yellow with black wings", "age": 2},
-    {"name": "Paul", "species": "House finch", "description": "Grey with red head and chest", "age": 5},
-    {"name": "Clint", "species": "Hawfinch", "description": "Orange with brown and black wings", "age": 1}
-]
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
+
 def about(request):
     return render(request, "about.html")
 
+
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, "finches/index.html", {"finches": finches})
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, "finches/detail.html", {"finch": finch})
